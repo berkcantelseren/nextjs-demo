793de70f8e2db7e0109de42bebb20da3dfa57f98
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-05 17:42:06 +0300

    group hover tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <a href="#" className="group ...">
+        <div>
+          <svg
+            className="stroke-sky-500 group-hover:stroke-white ..."
+            fill="none"
+            viewBox="0 0 24 24"
+          ></svg>
+          <h3 className="text-gray-900 group-hover:text-white ...">
+            New project
+          </h3>
+        </div>
+        <p className="text-gray-500 group-hover:text-white ...">
+          Create a new project from a variety of starting templates.
+        </p>
+      </a>
+    </div>
   );
 }
