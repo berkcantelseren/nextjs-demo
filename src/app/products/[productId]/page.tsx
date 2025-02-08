2c93c29a5beb683e32e3c13f71a639842244cc35
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-08 21:14:54 +0300

    dynamic metadata config

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,5 +1,16 @@
+import { Metadata } from "next";
+
 type Props = {
   params: Promise<{ productId: string }>;
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  return {
+    title: `Product ${id}`,
+  };
 };
 
 export default async function Product({ params }: Props) {
