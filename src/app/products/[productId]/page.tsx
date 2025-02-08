64dfbe02ce27b3e272b5853f6e91c66f99d21fc4
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-08 21:12:10 +0300

    add props productId

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,8 @@
-export default async function Product({
-  params,
-}: {
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+export default async function Product({ params }: Props) {
   const productId = (await params).productId;
   return <h1>Product: {productId}</h1>;
 }
