a4f51a9f7a25043cd824f387bda12329cf982244
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-09 21:24:25 +0300

    link for product and blog with next/link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,26 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <a href="#" className="group ...">
-        <div>
-          <svg
-            className="stroke-sky-500 group-hover:stroke-white ..."
-            fill="none"
-            viewBox="0 0 24 24"
-          ></svg>
-          <h3 className="text-red-300 group-hover:text-white ...">
-            New project
-          </h3>
-        </div>
-        <p className="text-red-300 group-hover:text-white ...">
-          Create a new project from a variety of starting templates.
-        </p>
-      </a>
-    </div>
+    <>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <a href="#" className="group ...">
+          <div>
+            <svg
+              className="stroke-sky-500 group-hover:stroke-white ..."
+              fill="none"
+              viewBox="0 0 24 24"
+            ></svg>
+            <h3 className="text-red-300 group-hover:text-white ...">
+              New project
+            </h3>
+          </div>
+          <p className="text-red-300 group-hover:text-white ...">
+            Create a new project from a variety of starting templates.
+          </p>
+        </a>
+        <Link href="/blog">Blog</Link>
+        <Link href="/products">Product</Link>
+      </div>
+    </>
   );
 }
