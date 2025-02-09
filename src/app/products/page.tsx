84fb32ab91c7ae62dfa2a1c77dfe8036691d578b
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-09 21:36:34 +0300

    replace and product links

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,14 +1,25 @@
+import Link from "next/link";
+
 export default function Products() {
   return (
     <>
       <h1>Products</h1>
-      <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-        <li>Product 4</li>
-        <li>Product 5</li>
-      </ul>
+
+      <h2 className="font-bold text-4xl ml-[800px] text-nowrap mt-20 ">
+        <Link href="/products/1">Product 1</Link>
+      </h2>
+      <h2 className="font-bold text-4xl ml-[800px] text-nowrap mt-20">
+        <Link href="/products/2">Product 2</Link>
+      </h2>
+      <h2 className="font-bold text-4xl ml-[800px] text-nowrap mt-20 mb-20">
+        <Link href="/products/3" replace>
+          Product 3
+        </Link>
+      </h2>
+
+      <Link href="/" className="font-bold text-4xl ml-[800px] text-nowrap ">
+        Return Home
+      </Link>
     </>
   );
 }
