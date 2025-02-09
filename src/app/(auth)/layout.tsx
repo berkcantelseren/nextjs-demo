c2982b0c506a82447d605a3be208599170777a02
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-09 22:24:39 +0300

    layout change

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
 
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
 export default function AuthLayout({
   children,
 }: {
