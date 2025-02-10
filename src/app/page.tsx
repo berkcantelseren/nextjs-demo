330c8366a933acde27b1be5a497bd7dac21a7354
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-10 21:18:25 +0300

    css add for links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
-      <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
+      <Link
+        className="mr-10 text-2xl text-blue-500 font-bold"
+        href="/articles/breaking-news-123?lang=en"
+      >
+        Read in English
+      </Link>
+      <Link
+        className="mr-10 text-2xl text-blue-500 font-bold"
+        href="/articles/breaking-news-123?lang=fr"
+      >
+        Read in French
+      </Link>
       <hr />
       <Link href="/blog">Blog</Link>
       <Link href="/products">Product</Link>
