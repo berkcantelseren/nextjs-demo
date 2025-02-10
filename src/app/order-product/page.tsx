44426618b1b7e565e3bea3c4bda2d14d28b1d8d1
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-10 21:45:46 +0300

    place order page create and use next/navigation

--- src/app/order-product/page.tsx
+++ src/app/order-product/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function OrderProduct() {
+  const router = useRouter();
   const handleClick = () => {
     console.log("placing your order.");
+    router.push("/");
   };
   return (
     <>
