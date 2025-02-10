ba1aa47fc501b86a4ae508f0d30ed8c4c26f316d
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-10 21:27:14 +0300

    params and searchParams add

--- src/app/articles/[articleId]/page.tsx
+++ src/app/articles/[articleId]/page.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
 
-export default function NewsArticle() {
+export default async function NewsArticle({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ articleId: string }>;
+  searchParams: Promise<{ lang?: "en" | "es" | "fr" }>;
+}) {
+  const { articleId } = await params;
+  const { lang = "en" } = await searchParams;
   return (
     <div>
-      <h1>News article id </h1>
-      <p>Reading in language</p>
+      <h1>News article {articleId} </h1>
+      <p>Reading in {lang}</p>
       <div>
-        <Link href="/articles/id?lang=en">English</Link>
-        <Link href="/articles/id?lang=es">Spanish</Link>
-        <Link href="/articles/id?lang=fr">French</Link>
+        <Link href={`/articles/${articleId}?lang=en`}>English</Link>
+        <Link href={`/articles/${articleId}?lang=es`}>Spanish</Link>
+        <Link href={`/articles/${articleId}?lang=fr`}>French</Link>
       </div>
     </div>
   );
