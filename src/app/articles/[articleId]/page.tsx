661ab45cbff7981d45255863a744a683410898c9
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-10 21:16:25 +0300

    newsarticle create for 3 lang.

---
+++ src/app/articles/[articleId]/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function NewsArticle() {
+  return (
+    <div>
+      <h1>News article id </h1>
+      <p>Reading in language</p>
+      <div>
+        <Link href="/articles/id?lang=en">English</Link>
+        <Link href="/articles/id?lang=es">Spanish</Link>
+        <Link href="/articles/id?lang=fr">French</Link>
+      </div>
+    </div>
+  );
+}
