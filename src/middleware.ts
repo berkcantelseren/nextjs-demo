504104c9f9f3ac5fc2ef4e09bb69d80fcd626c88
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-17 23:08:11 +0300

    add cookies to middleware / theme dark mode

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,14 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  return NextResponse.rewrite(new URL("/", request.url));
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get("theme");
+  if (!themePreference) {
+    response.cookies.set("theme", "dark");
+  }
+
+  return response;
+  //  return NextResponse.rewrite(new URL("/", request.url));
 }
 
 export const config = {
